c8fbce1d146725ee91111d0d787a383278a03b43
liuxiaohui <1406744100@qq.com>
2018-02-26 09:35:11 +0800

    utils.py ok

--- utils.py
+++ utils.py
@@ -16,8 +16,8 @@ def read_data(filename):
 
 
 def index_data(sentences, dictionary):
-    shape = sentences.shape
-    sentences = sentences.reshape([-1])
+    shape = np.shape(sentences)
+    sentences = np.reshape(sentences, [-1])
     index = np.zeros_like(sentences, dtype=np.int32)
     for i in range(len(sentences)):
         try:
@@ -28,61 +28,24 @@ def index_data(sentences, dictionary):
     return index.reshape(shape)
 
 
-def get_train_data(vocabulary, batch_size, num_steps):
-    skip_window = 1       # How many words to consider left and right.
-    num_skips = 1        # How many times to reuse an input to generate a label.
+def get_train_data(vocabulary, dictionary, batch_size, num_steps):
+    data_size = len(vocabulary)
+    data_partition_size = data_size // batch_size
+    raw_x = index_data(vocabulary, dictionary)
 
-    data, count, dictionary, reversed_dictionary = build_dataset(vocabulary, len(vocabulary))
-#partition raw data into batches and stak them vertically in a data matrix
-    #for step in range(num_steps):
-    batch, labels = generate_batch(data,batch_size*num_steps, num_skips, skip_window)
-    print ('batch')
-    print (batch.shape)
+    raw_y = index_data(vocabulary[1:], dictionary)
+    raw_y[-1] = len(dictionary)-1
+    data_x = np.zeros([batch_size, data_partition_size], dtype=np.int32)
+    data_y = np.zeros([batch_size, data_partition_size], dtype=np.int32)
+    for i in range(batch_size):
+        data_x[i] = raw_x[data_partition_size*i : data_partition_size*(i+1)]
+        data_y[i] = raw_y[data_partition_size*i : data_partition_size*(i+1)]
 
-    print ('labels')
-    print (labels.shape)
-
-    batch = np.reshape(batch,(batch_size,-1) )
-    labels = np.reshape(labels,(batch_size,-1))
-
-    print ('batch')
-    print (batch.shape)
-    print ('labels')
-    print (labels.shape)
-
-
-    yield(batch ,labels)
-        ##################
-        # Your Code here
-        ##################
-data_index = 0
-def generate_batch(data,batch_size, num_skips, skip_window):
-    global data_index
-    assert batch_size % num_skips == 0
-    assert num_skips <= 2 * skip_window
-    batch = np.ndarray(shape=(batch_size), dtype=np.int32)
-    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
-    span = 2 * skip_window + 1  # [ skip_window target skip_window ]
-    buffer = collections.deque(maxlen=span)
-    if data_index + span > len(data):
-        data_index = 0
-    buffer.extend(data[data_index:data_index + span])
-    data_index += span
-    for i in range(batch_size // num_skips):
-        context_words = [w for w in range(span) if w != skip_window]
-        words_to_use = random.sample(context_words, num_skips)
-        for j, context_word in enumerate(words_to_use):
-            batch[i * num_skips + j] = buffer[skip_window]
-            labels[i * num_skips + j, 0] = buffer[context_word]
-        if data_index == len(data):
-            buffer.extend(data[0:span])
-            data_index = span
-        else:
-            buffer.append(data[data_index])
-            data_index += 1
-    # Backtrack a little bit to avoid skipping words in the end of a batch
-    data_index = (data_index + len(data) - span) % len(data)
-    return batch, labels
+    epoch_size = data_partition_size // num_steps
+    for i in range(epoch_size):
+        x = data_x[:, num_steps*i : num_steps*(i+1)]
+        y = data_y[:, num_steps*i : num_steps*(i+1)]
+        yield(x, y)
 
 
 def build_dataset(words, n_words):
